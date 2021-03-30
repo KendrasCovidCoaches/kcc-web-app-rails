9f9a05189ae977aebfbb4826be4c27ec0a4302ed
allenorton <norton.allene@gmail.com>
2021-03-30 16:47:17 -0500

    sessions controller add

--- kcc_backend/config/routes.rb
+++ kcc_backend/config/routes.rb
@@ -24,7 +24,17 @@ Rails.application.routes.draw do
   # get '/admin/edit' => 'admin#edit_site', as: 'edit_site'
   # post '/admin/edit' => 'admin#edit_site'
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, 
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' 
+  }
   devise_scope :user do
     get '/users/p/:page' => 'users/registrations#index', as: 'users_with_pagination'
     get 'users', to: 'users/registrations#index', as: 'volunteers'
